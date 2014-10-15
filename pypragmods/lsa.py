84c556428ea449f3db804590f783fd50ab444bf9
Christopher Potts <cgpotts@gmail.com>
2014-10-15 16:18:32 -0700

    Improved functions and usability (I hope)

--- pypragmods/lsa.py
+++ pypragmods/lsa.py
@@ -11,15 +11,17 @@ import matplotlib
 import matplotlib.font_manager as font_manager
 
 
-class HurfordExperiment:
+class Experiment:
     def __init__(self,
                  n=3,
                  disjunction_cost=0.0,             
-                 lexical_costs={'A':0.0, 'B':0.0, 'C':0.0},
+                 lexical_costs={'A':0.0, 'B':0.0, 'X':0.0},
                  temperature=1.0,
                  alpha=1.0,
                  beta=1.0,
-                 baselexicon={'A': ['1'], 'B': ['2'], 'C':['1', '2']}):
+                 baselexicon={'A': ['1'], 'B': ['2'], 'X':['1', '2']},
+                 prior=None,
+                 lexprior = None):
         self.n = n
         self.disjunction_cost = disjunction_cost
         self.lexical_costs = lexical_costs
@@ -27,6 +29,8 @@ class HurfordExperiment:
         self.alpha = alpha
         self.beta = beta        
         self.baselexicon = baselexicon
+        self.prior = prior
+        self.lexprior = lexprior
 
     def build(self):
         lex = Lexica(baselexicon=self.baselexicon,
@@ -39,19 +43,25 @@ class HurfordExperiment:
         self.states = lex.states
         self.messages = lex.messages
 
+        if self.prior == None:
+            self.prior = np.repeat(1.0/len(self.states), len(self.states))
+
+        if self.lexprior == None:
+            self.lexprior = np.repeat(1.0/len(self.lexica), len(self.lexica))
+            
         self.model = Pragmod(lexica=self.lexica,
                              messages=self.messages,
                              meanings=self.states,
                              costs=lex.cost_vector(),
-                             prior=np.repeat(1.0/len(self.states), len(self.states)),
-                             lexprior=np.repeat(1.0/len(lex), len(lex)),
+                             prior=self.prior,
+                             lexprior=self.lexprior,
                              temperature=self.temperature,
                              alpha=self.alpha,
                              beta=self.beta)
 
-        self.langs = self.model.run_expertise_model(n=self.n, display=True)
+        self.langs = self.model.run_expertise_model(n=self.n, display=False)
 
-    def listener_inference(self, msg='A v C'):
+    def listener_inference(self, msg='A v X'):
         final_lis_lang = self.langs[-1]
         msg_index = self.messages.index(msg)
         prob_table = []
@@ -60,7 +70,7 @@ class HurfordExperiment:
             prob_table.append(row)
         return np.array(prob_table)
 
-    def display_listener_inference(self, msg='A v C'):
+    def display_listener_inference(self, msg='A v X'):
         print "--------------------------------------------------"
         print self.params2str()
         prob_table = self.listener_inference(msg=msg)        
@@ -86,7 +96,10 @@ class HurfordExperiment:
                     vals.append("%s: %s" % (params[x], getattr(self, x)))
         return joiner.join(vals)
 
-    def lex2str(self, lexicon):
+    def lex2str(self, lexicon_or_lexicon_index):
+        lexicon = lexicon_or_lexicon_index
+        if isinstance(lexicon, int):
+            lexicon = self.lexica[lexicon_or_lexicon_index]            
         def state_sorter(x):
             return sorted(x, cmp=(lambda x, y: cmp(len(x), len(y))))
         entries = []
@@ -96,95 +109,35 @@ class HurfordExperiment:
             entries.append(entry)
         return "; ".join(entries)
 
-    def plot_listener_inference_depth_values(self,
-                                             msg='A v C',
-                                             target_state='1 v 2',
-                                             n_values=np.arange(1, 5, 1),
-                                             legend_loc='upper right',
-                                             output_filename=None):
-        self.plot_listener_inference_parameter_space(msg=msg,
-                                                     target_state=target_state,
-                                                     parameter_name='n',
-                                                     parameter_text='Depth',
-                                                     parameter_values=n_values,
-                                                     legend_loc=legend_loc,
-                                                     output_filename=output_filename)
+    def plot_listener_inference_depth_values(self, msg='A v X', target_state='1 v 2', n_values=np.arange(1, 5, 1), legend_loc='upper right', output_filename=None):
+        self.plot_listener_inference_parameter_space(msg=msg, target_state=target_state, parameter_name='n', parameter_text='Depth', parameter_values=n_values, legend_loc=legend_loc, output_filename=output_filename)
 
-    def plot_listener_inference_beta_values(self,
-                                            msg='A v C',
-                                            target_state='1 v 2',                                                 
-                                            beta_values=np.arange(0.01, 5.0, 0.01),
-                                            legend_loc='upper right',
-                                            output_filename=None):
-        self.plot_listener_inference_parameter_space(msg=msg,
-                                                     target_state=target_state,
-                                                     parameter_name='beta',
-                                                     parameter_text=r"$\beta$",
-                                                     parameter_values=beta_values,
-                                                     legend_loc=legend_loc,
-                                                     output_filename=output_filename)
+    def plot_listener_inference_beta_values(self, msg='A v X', target_state='1 v 2', beta_values=np.arange(0.01, 5.0, 0.01), legend_loc='upper right', output_filename=None): 
+        self.plot_listener_inference_parameter_space(msg=msg, target_state=target_state, parameter_name='beta', parameter_text=r"$\beta$", parameter_values=beta_values, legend_loc=legend_loc, output_filename=output_filename)
 
-    def plot_listener_inference_alpha_values(self,
-                                            msg='A v C',
-                                            target_state='1 v 2',                                                 
-                                            alpha_values=np.arange(0.01, 5.0, 0.01),
-                                            legend_loc='upper right',
-                                            output_filename=None):
-        self.plot_listener_inference_parameter_space(msg=msg,
-                                                     target_state=target_state,
-                                                     parameter_name='alpha',
-                                                     parameter_text=r"$\alpha$",
-                                                     parameter_values=alpha_values,
-                                                     legend_loc=legend_loc,
-                                                     output_filename=output_filename)
+    def plot_listener_inference_alpha_values(self, msg='A v X', target_state='1 v 2', alpha_values=np.arange(0.01, 5.0, 0.01), legend_loc='upper right', output_filename=None):
+        self.plot_listener_inference_parameter_space(msg=msg, target_state=target_state, parameter_name='alpha', parameter_text=r"$\alpha$", parameter_values=alpha_values, legend_loc=legend_loc, output_filename=output_filename)
 
-    def plot_listener_inference_disjunction_costs(self,
-                                                  msg='A v C',
-                                                  target_state='1 v 2',                                                 
-                                                  disjunction_cost_values=np.arange(0.0, 5.0, 0.01),
-                                                  legend_loc='upper right',
-                                                  output_filename=None):
-        self.plot_listener_inference_parameter_space(msg=msg,
-                                                     target_state=target_state,
-                                                     parameter_name='disjunction_cost',
-                                                     parameter_values=disjunction_cost_values,
-                                                     legend_loc=legend_loc,
-                                                     output_filename=output_filename)
+    def plot_listener_inference_disjunction_costs(self, msg='A v X', target_state='1 v 2', disjunction_cost_values=np.arange(0.0, 5.0, 0.01), legend_loc='upper right', output_filename=None): 
+        self.plot_listener_inference_parameter_space(msg=msg, target_state=target_state, parameter_name='disjunction_cost', parameter_values=disjunction_cost_values, legend_loc=legend_loc, output_filename=output_filename)
 
-    def plot_listener_inference_lambda_values(self,
-                                              msg='A v C',
-                                              target_state='1 v 2',
-                                              lambda_values=np.arange(0.01, 5.0, 0.01),
-                                              legend_loc='upper right',
-                                              output_filename=None):
-        self.plot_listener_inference_parameter_space(msg=msg,
-                                                     target_state=target_state,
-                                                     parameter_name='temperature',
-                                                     parameter_text=r"$\lambda$",
-                                                     parameter_values=lambda_values,
-                                                     legend_loc=legend_loc,
-                                                     output_filename=output_filename)
+    def plot_listener_inference_lambda_values(self, msg='A v X', target_state='1 v 2', lambda_values=np.arange(0.01, 5.0, 0.01), legend_loc='upper right', output_filename=None): 
+        self.plot_listener_inference_parameter_space(msg=msg, target_state=target_state, parameter_name='temperature', parameter_text=r"$\lambda$", parameter_values=lambda_values, legend_loc=legend_loc, output_filename=output_filename)
        
 
     def plot_listener_inference_parameter_space(self,
-                                                msg='A v C',
+                                                msg='A v X',
                                                 target_state='1 v 2',
                                                 parameter_name='disjunction_cost',
                                                 parameter_text=None,
                                                 parameter_values=np.arange(0.0, 5.0, 0.01),
                                                 legend_loc='upper right',
                                                 output_filename=None):
-        if parameter_text == None: parameter_text = parameter_name
-        # Plotting set-up:
-        fontname='/Library/Fonts/Arial.ttf'
-        if os.uname()[1] == 'lingpc29.ucsd.edu':
-            fontname='/Library/Fonts/Microsoft/Arial.ttf'
-        prop = font_manager.FontProperties(fname=fontname)
-        matplotlib.rcParams['font.family'] = prop.get_name()
+        if parameter_text == None: parameter_text = parameter_name        
         setup = {'family': 'sans-serif', 'sans-serif':['Helvetica'], 'weight':'normal', 'size':18}
         title_size = 18
         axis_label_size = 18
-        #matplotlib.rc('font', **setup) 
+        matplotlib.rc('font', **setup) 
         matplotlib.rc('xtick', labelsize=14)
         matplotlib.rc('ytick', labelsize=14)
         matplotlib.rcParams.update({'font.size': 12})
@@ -211,6 +164,7 @@ class HurfordExperiment:
             paramvals, vals, maxval_markers = zip(*probs[lex_index])
             lex_rep = self.lex2str(self.lexica[lex_index])
             plt.plot(paramvals, vals, marker="", linestyle="-", label=lex_rep, color=colors[lex_index], linewidth=3)
+            # Dots mark max-values in the joint table --- best inferences for the listener:
             dots = [(paramval, val) for paramval, val, marker in probs[lex_index] if marker]
             if dots:           
                 dotsx, dotsy = zip(*dots)
@@ -227,79 +181,37 @@ class HurfordExperiment:
         else:
             plt.show()
 
-def explore_hyperparameters(msg='A v C', target_state='1 v 2', target_lex_index=0):    
-    temps = np.arange(0.0, 3.0, 0.2)
-    dcosts = np.arange(0.0, 3.0, 0.2)
-    alphas = np.arange(0.0, 3.0, 0.2)
-    betas = np.arange(0.0, 3.0, 0.2)
+def explore_hyperparameters(baselexicon={'A': ['1'], 'B': ['2'], 'X':['1', '2']},
+                            lexical_costs={'A':0.0, 'B':0.0, 'X':0.0},
+                            msg='A v X'):    
+    temps = np.arange(0.0, 3.2, 0.2)
+    dcosts = np.arange(0.0, 3.2, 0.2)
+    alphas = np.arange(0.0, 3.2, 0.2)
+    betas = np.arange(0.0, 3.2, 0.2)
     depths = [3]
-    results = []
+    results = defaultdict(list)
     for temp, dcost, alpha, beta, depth in product(temps, dcosts, alphas, betas, depths):
-        params = [temp, dcost, alpha, beta, depth]
-        hurford = HurfordExperiment(temperature=temp, disjunction_cost=dcost, beta=beta, alpha=alpha)
-        hurford.build()
-        target_state_index = hurford.states.index(target_state)
-        prob_table = hurford.listener_inference(msg=msg)
-        prob_table_sort = np.sort(prob_table.flatten())
-        prob_table_max = prob_table_sort[-1]
-        if prob_table_max == prob_table_sort[-2]:
-            prob_table_max = None
-        prob = prob_table[target_lex_index][target_state_index]
-        result = False
-        if prob == prob_table_max:
-            result = True
-        params.append(result)
-        print params
-        results.append(params)
-    output_filename = "paramexplore-%s_%s_%s.csv" % (msg.replace(" ", ""), target_state.replace(" ", ""), target_lex_index)
-    w = csv.writer(file(output_filename, 'w'))
-    w.writerow(['lambda', 'dcost', 'alpha', 'beta', 'depth', 'outcome'])
-    w.writerows(results)
+        params = {'lambda': temp, 'alpha': alpha, 'beta': beta, 'depth': depth, 'disjunction_cost': dcost}
+        experiment = Experiment(baselexicon=baselexicon, lexical_costs=lexical_costs, n=depth, temperature=temp, alpha=alpha, beta=beta, disjunction_cost=dcost)
+        experiment.build()
+        prob_table = experiment.listener_inference(msg=msg)
+        sorted_probs = sorted(prob_table.flatten())
+        max_pair = None
+        max_prob = sorted_probs[-1]
+        # No ties allowed!
+        if max_prob != sorted_probs[-2]:
+            for i, j in product(range(prob_table.shape[0]), range(prob_table.shape[1])):
+                if prob_table[i, j] == max_prob:
+                    max_pair = (i, experiment.states[j])
+        #if max_pair == (0, '1'):
+        print max_pair, params
+        results[max_pair].append(params)
+    return results
 
-                    
+
 if __name__ == '__main__':
-
-    # hurford = HurfordExperiment(temperature=2.0, disjunction_cost=1.0, beta=1.0, alpha=1.0)
-    # hurford.build()
-    # for i, lex in enumerate(hurford.lexica):
-    #     print "======================================================================"
-    #     print "Lex%s" % i
-    #     lis0 = hurford.model.l0(lex)
-    #     display_matrix(lis0, rnames=hurford.messages, cnames=hurford.states)
-    #     spk1 = hurford.model.s1(lex)
-    #     display_matrix(spk1, cnames=hurford.messages, rnames=hurford.states)
-    #     lis1 = hurford.model.l1(lex)
-    #     display_matrix(lis1, rnames=hurford.messages, cnames=hurford.states)
-
-    hurford = HurfordExperiment(temperature=2.0, disjunction_cost=2.0, beta=1.0, alpha=1.0)
-    # hurford.build()
-    # hurford.display_listener_inference(msg='A v C')
-    # hurford.plot_listener_inference_disjunction_costs()
-    # hurford.plot_listener_inference_disjunction_costs(msg='A v C', target_state='1', legend_loc='lower right')
-    # hurford.plot_listener_inference_beta_values(msg='A v C', target_state='1')
-    # hurford.plot_listener_inference_lambda_values(msg='A v C', target_state='1 v 2', output_filename='temp.pdf')
-    hurford.plot_listener_inference_depth_values(msg='A v C', target_state='1', legend_loc='right', output_filename='temp.pdf', n_values=[1])
-
-    # hurford = HurfordExperiment(n=3,
-    #                             temperature=1.0,
-    #                             beta=0.0,
-    #                             disjunction_cost=0.01,
-    #                             baselexicon={'A': ['1'], 'B': ['2'], 'C': ['3'], 'D': ['4'], 'X': ['1', '2', '3', '4']},
-    #                             lexical_costs={'A':0.0, 'B':0.0, 'C':0.0, 'D':0.0, 'X':0.0})
     
-    # hurford.build()
-    # hurford.display_listener_inference(msg='A v X')
-    # hurford.plot_listener_inference_disjunction_costs(msg='A v X', target_state='1', legend_loc='right')
-    # print display_matrix(hurford.lexica[0], rnames=hurford.messages, cnames=hurford.states)
-    # hurford.plot_listener_inference_lambda_values(msg='A v X', target_state='1', legend_loc='right')
-    # hurford.plot_listener_inference_beta_values(msg='A v X', target_state='1', legend_loc='right')
-    # hurford.plot_listener_inference_alpha_values(msg='A v X', target_state='1 v 2', legend_loc='right')
-    # hurford.plot_listener_inference_lambda_values(msg='A v X', target_state='1 v 2', lambda_values=np.arange(0.0, 5.0, 0.1))
-
-    # Hurfordian:
-    # explore_hyperparameters(msg='A v C', target_state='1 v 2', target_lex_index=1)
-
-    # explore_hyperparameters(msg='A v C', target_state='1', target_lex_index=0)
-    
-
-    
+    hurford = Experiment(temperature=1.0, disjunction_cost=0.01, beta=1.0, alpha=1.0)
+    hurford.build()
+    hurford.display_listener_inference(msg='A v X')
+       
