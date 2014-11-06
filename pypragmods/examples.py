5525d10cea16b59b11b35c18aa374625d2dcaf62
Christopher Potts <cgpotts@gmail.com>
2014-11-06 15:02:16 -0800

    Disjunction example

--- pypragmods/examples.py
+++ pypragmods/examples.py
@@ -42,16 +42,21 @@ def manner(n=3):
 
 def disjunction(n=3):
     """Settings for Bergen et al.'s figure 6. Seems to reproduce the effects they report."""
-    lexica = Lexica(baselexicon={'p': [r'$w_1$', r'$w_2$'], 'q':[r'$w_2$', r'$w_3$']},
-                    disjunction_cost=0.1,
-                    conjunction_cost=0.1,
+   # bl = {'p': [r'$w_1$', r'$w_2$'], 'q':[r'$w_2$', r'$w_3$']}
+    bl = {'p': ['1', '2'], 'q':['2', '3']}
+    lexica = Lexica(baselexicon=bl,
+                    atomic_states=['1', '2', '3'],
+                    disjunction_cost=1.0,
+                    conjunction_cost=0.0,
                     null_cost=5.0,                    
                     join_closure=True,
-                    meet_closure=False,
+                    meet_closure=True,
                     block_trivial_messages=True,
                     block_ineffability=False)
     
-    lexica.display()   
+    lexica.display()
+    for key, val in lexica.lexica[8].items():
+        print key, len(val), val
     mod = Pragmod(lexica=lexica.lexica2matrices(),
                   messages=lexica.messages,
                   meanings=lexica.states,
@@ -59,10 +64,10 @@ def disjunction(n=3):
                   prior=np.repeat(1.0/len(lexica.states), len(lexica.states)),
                   lexprior=np.repeat(1.0/len(lexica), len(lexica)),
                   temperature=1.0,
-                  alpha=5.0)
-    mod.plot_expertise_listener(output_filename='../paper/fig/scalardisj-expertise-listener-marginalized.pdf', n=n)
-    mod.plot_expertise_speaker(output_filename='../paper/fig/scalardisj-expertise-speaker.pdf', n=n)
-    mod.plot_expertise_speaker(output_filename='../paper/fig/scalardisj-expertise-speaker-lexsum.pdf', n=n, lexsum=True)
+                  alpha=1.0)
+    #mod.plot_expertise_listener(output_filename='../paper/fig/scalardisj-expertise-listener-marginalized.pdf', n=n)
+    #mod.plot_expertise_speaker(output_filename='../paper/fig/scalardisj-expertise-speaker.pdf', n=n)
+    #mod.plot_expertise_speaker(output_filename='../paper/fig/scalardisj-expertise-speaker-lexsum.pdf', n=n, lexsum=True)
 
 
         
@@ -70,6 +75,6 @@ def disjunction(n=3):
 if __name__ == '__main__':    
 
     #scalars(n=3)
-    manner(n=3)
-    #disjunction(n=3)
+    #manner(n=3)
+    disjunction(n=2)
 
